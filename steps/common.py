19767ed29cd5a10bd08b91cc47d6df83ae160dfc
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-09-13 12:47:56 -0700

    minor change to common file

--- steps/common.py
+++ steps/common.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC, expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait as wait
 
 
@@ -16,3 +16,8 @@ def welcome_message_method(driver):
 
 def error_message_method(driver):
     return driver.find_element_by_id('spanMessage').text
+
+
+def logout(driver):
+    driver.find_element_by_id('welcome').click()
+    wait(driver, 2).until(expected_conditions.visibility_of_element_located((By.LINK_TEXT, 'Logout'))).click()
