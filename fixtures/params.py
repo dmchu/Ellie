42518c6b9a8513992900db55922e44cea3e24fc8
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-09-16 13:23:43 -0700

    created fixtures/params

---
+++ fixtures/params.py
@@ -0,0 +1,11 @@
+import os
+
+cwd = os.path.abspath(os.getcwd())
+
+DOMAIN = 'http://hrm.seleniumminutes.com'
+DEFAULT_PASSWORD = 'Password'
+BROWSER_TYPE = 'Chrome'
+CHROME_EXPATH = os.path.join(cwd, 'browser_drivers/chromedriver')
+FIREFOX_EXPATH = os.path.join(cwd, 'browser_drivers/geckodriver')
+EXPLICIT_TIMEOUT = 2
+# SLOW_TIMEOUT = 10  example of other timeouts
