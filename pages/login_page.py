3692560ff47825e7890c18cc2191eb65968026fc
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-09-16 13:24:32 -0700

    added parameters from params

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,19 @@
+from fixtures.params import DOMAIN, DEFAULT_PASSWORD
+
+
 class LoginPage():
     def __init__(self,driver):
         self.driver = driver
 
 
-    def login(self, username='admin', password='Password'):
+    def login(self, username='admin', password=DEFAULT_PASSWORD):
         driver = self.driver
         driver.find_element_by_id('txtUsername').send_keys(username)
         driver.find_element_by_id('txtPassword').send_keys(password)
         driver.find_element_by_id('btnLogin').click()
+
+    def goto_login_page(self):
+        # go to the admin page
+        self.driver.get(DOMAIN)
+        # self.driver.get('http://hrm.seleniumminutes.com/symfony/web/index.php/auth/login')
+
